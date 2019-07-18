4d3fb7795e413348a187c215fb4267eb455ba4c3
AngelVil92 <angelvil92@gmail.com>
2019-07-18 11:22:42 +0200

    Ejemplo de generar paginas estaticas

---
+++ info/pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h2>Sobre mi</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum itaque omnis maiores, repellat laudantium officiis obcaecati provident. Hic ipsum, error, delectus, cupiditate aut excepturi nemo quia fugiat molestiae amet eius?</p>
+  </div>
+</template>
---
+++ info/pages/contactame.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h2>Contactame</h2>
+    <p>Email: ...</p>
+    <p>Linkedin: ...</p>
+    <p>Twitter: ...</p>
+  </div>
+</template>
